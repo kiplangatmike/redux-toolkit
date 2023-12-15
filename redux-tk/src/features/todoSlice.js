b3e43abef3ce5228873385cf114044f4da230f75
kiplangatmike <m.kiplangat@alustudent.com>
2023-12-15 23:57:46 +0200

    improvements on todo slice

--- redux-tk/src/features/todoSlice.js
+++ redux-tk/src/features/todoSlice.js
@@ -1,12 +1,13 @@
 import {createSlice, nanoid} from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{id: 1, text: "hello"}]
+    todos: [{id: 1, text: "hello" }]
 }
 
 export const todoSlice = createSlice({
     name: 'todos',
     initialState,
+    
     reducers: {
         addTodo: (state, action) => {
             const todo = {
